9c8865f14523d2a3ac72eb352a22c89db4cc437e
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-03-16 20:42:32 +0300

    3

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,12 @@
 int cbinsearch(int *arr, int size, int value){
   int lover=0;
   int high=size -1;
-  while(lower<high){
+  while (lower<high){
     int mid=(lower+high)/2;
-    if(arr[mid]<value);
+    if (arr[mid]<value);
     lower=mid++;
     else if (arr[mid]>value)high=mid;
-  }else{
+  } else {
     return mid;}
   return 0; // если ничего не найдено
 }
