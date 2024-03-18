38c36a214180f0ba7261cc01aa284e0297fda25a
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-03-18 21:51:09 +0300

    12

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,36 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int lower = 0;
-  int high = size - 1;
-  while (lower <= high) {
-    int mid = (lower + high) / 2;
-    if (arr[mid] < value) {
-      lower = mid + 1;
-    } else if (arr[mid] > value) {
-      high = mid - 1;
-    } else {
-      return mid;
+    int left = 0;
+    int right = size - 1;
+    int count = 0;
+    while (left <= right) {
+        int mid = left + (right - left) / 2; 
+        if (arr[mid] == value) {
+            count++;
+            for (int i = mid - 1; i >= left; i--) {
+                if (arr[i] == value) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            for (int i = mid + 1; i <= right; i++) {
+                if (arr[i] == value) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+
+            break;
+        }
+        else if (arr[mid] < value) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
     }
-  }
-  return 0;
+    return count;
 }
